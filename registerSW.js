6b76a20318c36e97f2e97543bba5ed847de4ff01
skandar-uvsq <skandar.chahbouni@ens.uvsq.fr>
2024-10-07 18:28:33 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/skandar-uvsq.github.io/sw.js', { scope: '/skandar-uvsq.github.io/' })})}
